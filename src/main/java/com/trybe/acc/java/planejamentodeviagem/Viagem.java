0be9b125d5e12963f5020ce44eb423322a8e16ee
Lucas <lucaseduardolema@gmail.com>
2023-05-12 14:21:17 -0300

    add files

---
+++ src/main/java/com/trybe/acc/java/planejamentodeviagem/Viagem.java
@@ -0,0 +1,48 @@
+package com.trybe.acc.java.planejamentodeviagem;
+
+public class Viagem {
+  private String embarque;
+  private String origem;
+  private String destino;
+  private double distanciaKm;
+  private Voo voo;
+
+  /**
+   * Método construtor da classe.
+   * 
+   */
+  public Viagem(String partida, String origem, String destino, double distanciaKm) {
+    this.embarque = partida;
+    this.origem = origem;
+    this.destino = destino;
+    this.distanciaKm = distanciaKm;
+    this.voo = new Voo();
+  }
+
+  /**
+   * Método para retorno do horário de Desembarque no Destino.
+   * 
+   */
+  public String retonarDesembarqueHorarioLocalDestino() {
+    Tempo tempoViagem =
+        new Tempo(this.embarque, this.origem, this.destino, this.retornarDuracaoVoo());
+    return tempoViagem.retonarDesembarqueHorarioLocalDestino();
+  }
+
+  // aqui consideramos que um aviao percorre em media 700 km a cada hora
+  public int retornarDuracaoVoo() {
+    return this.voo.retornarTempoVoo(this.distanciaKm);
+  }
+
+  /**
+   * Método para retorno da informaçao da viagem.
+   * 
+   */
+  public String retornarInformacaoViagem() {
+    Tempo tempoViagem =
+        new Tempo(this.embarque, this.origem, this.destino, this.retornarDuracaoVoo());
+
+    return "Partida: " + this.embarque + "\n" + "Origem: " + this.origem + "\n\n" + "Chegada: "
+        + tempoViagem.retonarDesembarqueHorarioLocalDestino() + "\n" + "Destino: " + this.destino;
+  }
+}
---
+++ src/main/java/com/trybe/acc/java/planejamentodeviagem/Voo.java
@@ -0,0 +1,31 @@
+package com.trybe.acc.java.planejamentodeviagem;
+
+/**
+ * .
+ */
+public class Voo {
+  private int tempoVoo;
+
+  /**
+   * Método para retornar tempo de Voo.
+   * 
+   */
+  public int retornarTempoVoo(double distanciaKm) {
+
+    Double duracaoDouble = distanciaKm / 700.0;
+
+    this.tempoVoo = duracaoDouble.intValue();
+
+    return duracaoDouble.intValue();
+  }
+
+  /**
+   * Método para retornar informaçao do Voo.
+   * 
+   */
+  public String retornarInformacaoVoo(String embarque, String origem, String desembarque,
+      String destino) {
+    /* Implemente sua solução aqui */
+    return "test";
+  }
+}
